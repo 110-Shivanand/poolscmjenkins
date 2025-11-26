eb7cd491cbaac56e13ebc98ea93a61f944bf990e
shivanand <shivanand1807961@gmail.com>
2025-11-26 13:44:24 +0530

    mn

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world from jenkins pool scm");
+        System.out.println("Hello world from jenkins pool  web hook scm");
         int m=10;
     } 
 }
