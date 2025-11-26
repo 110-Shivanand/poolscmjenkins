a021bbe1af5b7d6775fd3c4e4db3172ff81bf7e0
shivanand <shivanand1807961@gmail.com>
2025-11-26 11:36:30 +0530

    n

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, shiva World! shiva");
-    }
+        System.out.println("Hello world from jenkins");
+        int m=10;
+    } 
 }
